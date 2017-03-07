7b26e62168fa1810deaf34f0f525293f39a4985e
Sergio <sergio@MacBook-Pro-de-Sergio.local>
2017-03-07 16:02:24 +0100

    Creada pagina de registro
    Creada la pagina de registro y añadido el logo oficial

--- template-sin_regsitro.php
+++ template-sin_regsitro.php
@@ -49,218 +49,17 @@
 			<header class="header">
 				<div class="logo-container">
 					<a href="../" class="logo">
-						<img src="assets/images/logo.png" height="35" alt="Porto Admin" />
+						<img src="assets/images/logo.png" height="35" alt="BlaBlaPet" />
 					</a>
-					<div class="visible-xs toggle-sidebar-left" data-toggle-class="sidebar-left-opened" data-target="html" data-fire-event="sidebar-left-opened">
-						<i class="fa fa-bars" aria-label="Toggle sidebar"></i>
-					</div>
 				</div>
 			
 				<!-- start: search & user box -->
 				<div class="header-right">
 			
-					<form action="pages-search-results.html" class="search nav-form">
-						<div class="input-group input-search">
-							<input type="text" class="form-control" name="q" id="q" placeholder="Search...">
-							<span class="input-group-btn">
-								<button class="btn btn-default" type="submit"><i class="fa fa-search"></i></button>
-							</span>
-						</div>
-					</form>
-			
 					<span class="separator"></span>
-			
-					<ul class="notifications">
-						<li>
-							<a href="#" class="dropdown-toggle notification-icon" data-toggle="dropdown">
-								<i class="fa fa-tasks"></i>
-								<span class="badge">3</span>
-							</a>
-			
-							<div class="dropdown-menu notification-menu large">
-								<div class="notification-title">
-									<span class="pull-right label label-default">3</span>
-									Tasks
-								</div>
-			
-								<div class="content">
-									<ul>
-										<li>
-											<p class="clearfix mb-xs">
-												<span class="message pull-left">Generating Sales Report</span>
-												<span class="message pull-right text-dark">60%</span>
-											</p>
-											<div class="progress progress-xs light">
-												<div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;"></div>
-											</div>
-										</li>
-			
-										<li>
-											<p class="clearfix mb-xs">
-												<span class="message pull-left">Importing Contacts</span>
-												<span class="message pull-right text-dark">98%</span>
-											</p>
-											<div class="progress progress-xs light">
-												<div class="progress-bar" role="progressbar" aria-valuenow="98" aria-valuemin="0" aria-valuemax="100" style="width: 98%;"></div>
-											</div>
-										</li>
-			
-										<li>
-											<p class="clearfix mb-xs">
-												<span class="message pull-left">Uploading something big</span>
-												<span class="message pull-right text-dark">33%</span>
-											</p>
-											<div class="progress progress-xs light mb-xs">
-												<div class="progress-bar" role="progressbar" aria-valuenow="33" aria-valuemin="0" aria-valuemax="100" style="width: 33%;"></div>
-											</div>
-										</li>
-									</ul>
-								</div>
-							</div>
-						</li>
-						<li>
-							<a href="#" class="dropdown-toggle notification-icon" data-toggle="dropdown">
-								<i class="fa fa-envelope"></i>
-								<span class="badge">4</span>
-							</a>
-			
-							<div class="dropdown-menu notification-menu">
-								<div class="notification-title">
-									<span class="pull-right label label-default">230</span>
-									Messages
-								</div>
-			
-								<div class="content">
-									<ul>
-										<li>
-											<a href="#" class="clearfix">
-												<figure class="image">
-													<img src="assets/images/!sample-user.jpg" alt="Joseph Doe Junior" class="img-circle" />
-												</figure>
-												<span class="title">Joseph Doe</span>
-												<span class="message">Lorem ipsum dolor sit.</span>
-											</a>
-										</li>
-										<li>
-											<a href="#" class="clearfix">
-												<figure class="image">
-													<img src="assets/images/!sample-user.jpg" alt="Joseph Junior" class="img-circle" />
-												</figure>
-												<span class="title">Joseph Junior</span>
-												<span class="message truncate">Truncated message. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sit amet lacinia orci. Proin vestibulum eget risus non luctus. Nunc cursus lacinia lacinia. Nulla molestie malesuada est ac tincidunt. Quisque eget convallis diam, nec venenatis risus. Vestibulum blandit faucibus est et malesuada. Sed interdum cursus dui nec venenatis. Pellentesque non nisi lobortis, rutrum eros ut, convallis nisi. Sed tellus turpis, dignissim sit amet tristique quis, pretium id est. Sed aliquam diam diam, sit amet faucibus tellus ultricies eu. Aliquam lacinia nibh a metus bibendum, eu commodo eros commodo. Sed commodo molestie elit, a molestie lacus porttitor id. Donec facilisis varius sapien, ac fringilla velit porttitor et. Nam tincidunt gravida dui, sed pharetra odio pharetra nec. Duis consectetur venenatis pharetra. Vestibulum egestas nisi quis elementum elementum.</span>
-											</a>
-										</li>
-										<li>
-											<a href="#" class="clearfix">
-												<figure class="image">
-													<img src="assets/images/!sample-user.jpg" alt="Joe Junior" class="img-circle" />
-												</figure>
-												<span class="title">Joe Junior</span>
-												<span class="message">Lorem ipsum dolor sit.</span>
-											</a>
-										</li>
-										<li>
-											<a href="#" class="clearfix">
-												<figure class="image">
-													<img src="assets/images/!sample-user.jpg" alt="Joseph Junior" class="img-circle" />
-												</figure>
-												<span class="title">Joseph Junior</span>
-												<span class="message">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sit amet lacinia orci. Proin vestibulum eget risus non luctus. Nunc cursus lacinia lacinia. Nulla molestie malesuada est ac tincidunt. Quisque eget convallis diam.</span>
-											</a>
-										</li>
-									</ul>
-			
-									<hr />
-			
-									<div class="text-right">
-										<a href="#" class="view-more">View All</a>
-									</div>
-								</div>
-							</div>
-						</li>
-						<li>
-							<a href="#" class="dropdown-toggle notification-icon" data-toggle="dropdown">
-								<i class="fa fa-bell"></i>
-								<span class="badge">3</span>
-							</a>
-			
-							<div class="dropdown-menu notification-menu">
-								<div class="notification-title">
-									<span class="pull-right label label-default">3</span>
-									Alerts
-								</div>
-			
-								<div class="content">
-									<ul>
-										<li>
-											<a href="#" class="clearfix">
-												<div class="image">
-													<i class="fa fa-thumbs-down bg-danger"></i>
-												</div>
-												<span class="title">Server is Down!</span>
-												<span class="message">Just now</span>
-											</a>
-										</li>
-										<li>
-											<a href="#" class="clearfix">
-												<div class="image">
-													<i class="fa fa-lock bg-warning"></i>
-												</div>
-												<span class="title">User Locked</span>
-												<span class="message">15 minutes ago</span>
-											</a>
-										</li>
-										<li>
-											<a href="#" class="clearfix">
-												<div class="image">
-													<i class="fa fa-signal bg-success"></i>
-												</div>
-												<span class="title">Connection Restaured</span>
-												<span class="message">10/10/2016</span>
-											</a>
-										</li>
-									</ul>
-			
-									<hr />
-			
-									<div class="text-right">
-										<a href="#" class="view-more">View All</a>
-									</div>
-								</div>
-							</div>
-						</li>
-					</ul>
-			
-					<span class="separator"></span>
-			
-					<div id="userbox" class="userbox">
-						<a href="#" data-toggle="dropdown">
-							<figure class="profile-picture">
-								<img src="assets/images/!logged-user.jpg" alt="Joseph Doe" class="img-circle" data-lock-picture="assets/images/!logged-user.jpg" />
-							</figure>
-							<div class="profile-info" data-lock-name="John Doe" data-lock-email="johndoe@okler.com">
-								<span class="name">John Doe Junior</span>
-								<span class="role">administrator</span>
-							</div>
-			
-							<i class="fa custom-caret"></i>
-						</a>
-			
-						<div class="dropdown-menu">
-							<ul class="list-unstyled">
-								<li class="divider"></li>
-								<li>
-									<a role="menuitem" tabindex="-1" href="pages-user-profile.html"><i class="fa fa-user"></i> My Profile</a>
-								</li>
-								<li>
-									<a role="menuitem" tabindex="-1" href="#" data-lock-screen="true"><i class="fa fa-lock"></i> Lock Screen</a>
-								</li>
-								<li>
-									<a role="menuitem" tabindex="-1" href="pages-signin.html"><i class="fa fa-power-off"></i> Logout</a>
-								</li>
-							</ul>
-						</div>
-					</div>
+                    <button type="submit" class="btn btn-primary">Inicia Sesión</button>
+                    <span class="separator"></span>
+                
 				</div>
 				<!-- end: search & user box -->
 			</header>
@@ -871,69 +670,80 @@
 
 				<section class="body-sign">
                     <div class="center-sign">
+                        
                         <a href="/" class="logo pull-left">
-                            <img src="assets/images/logo.png" height="54" alt="Porto Admin" />
+                            <img src="assets/images/logo.png" height="70" alt="BlaBlaPet" />
                         </a>
 
                         <div class="panel panel-sign">
                             <div class="panel-title-sign mt-xl text-right">
-                                <h2 class="title text-uppercase text-weight-bold m-none"><i class="fa fa-user mr-xs"></i> Sign Up</h2>
+                                <h2 class="title text-uppercase text-weight-bold m-none"><i class="fa fa-user mr-xs"></i> Registrate </h2>
                             </div>
                             <div class="panel-body">
                                 <form>
                                     <div class="form-group mb-lg">
-                                        <label>Name</label>
+                                        <label>Nombre</label>
                                         <input name="name" type="text" class="form-control input-lg" />
                                     </div>
 
                                     <div class="form-group mb-lg">
-                                        <label>E-mail Address</label>
+                                        <label>E-mail</label>
                                         <input name="email" type="email" class="form-control input-lg" />
                                     </div>
 
                                     <div class="form-group mb-none">
                                         <div class="row">
                                             <div class="col-sm-6 mb-lg">
-                                                <label>Password</label>
+                                                <label>Contraseña</label>
                                                 <input name="pwd" type="password" class="form-control input-lg" />
                                             </div>
                                             <div class="col-sm-6 mb-lg">
-                                                <label>Password Confirmation</label>
+                                                <label>Confirmar contraseña</label>
                                                 <input name="pwd_confirm" type="password" class="form-control input-lg" />
                                             </div>
                                         </div>
                                     </div>
-
+                                    
+                                    <div class="form-group mb-lg">
+                                        <label>Provincia</label>
+                                        <div class="input-group input-group-icon">
+                                            <input name="state" type="text" class="form-control input-lg" />
+                                            <span class="input-group-addon">
+                                                <span class="icon"><i class="fa fa-question-circle "></i></span>
+                                            </span>
+                                        </div>
+                                    </div>
+                                    
+                                    <div class="form-group mb-lg">
+                                        <label>Ciudad</label>
+                                        <input name="city" type="text" class="form-control input-lg" />
+                                    </div>
+                                    
                                     <div class="row">
                                         <div class="col-sm-8">
                                             <div class="checkbox-custom checkbox-default">
                                                 <input id="AgreeTerms" name="agreeterms" type="checkbox"/>
-                                                <label for="AgreeTerms">I agree with <a href="#">terms of use</a></label>
+                                                <label for="AgreeTerms">Acepto los <a href="#">terminos de uso</a></label>
                                             </div>
                                         </div>
                                         <div class="col-sm-4 text-right">
-                                            <button type="submit" class="btn btn-primary hidden-xs">Sign Up</button>
-                                            <button type="submit" class="btn btn-primary btn-block btn-lg visible-xs mt-lg">Sign Up</button>
+                                            <button type="submit" class="btn btn-primary hidden-xs">Confirmar</button>
+                                            <button type="submit" class="btn btn-primary btn-block btn-lg visible-xs mt-lg">Confirmar</button>
                                         </div>
                                     </div>
 
-                                    <span class="mt-lg mb-lg line-thru text-center text-uppercase">
-                                        <span>or</span>
-                                    </span>
-
-                                    <div class="mb-xs text-center">
-                                        <a class="btn btn-facebook mb-md ml-xs mr-xs">Connect with <i class="fa fa-facebook"></i></a>
-                                        <a class="btn btn-twitter mb-md ml-xs mr-xs">Connect with <i class="fa fa-twitter"></i></a>
-                                    </div>
-
-                                    <p class="text-center">Already have an account? <a href="pages-signin.html">Sign In!</a></p>
+                                    <p class="text-center">¿Ya tienes una cuenta? <a href="pages-signin.html">Inicia Sesión!</a></p>
 
                                 </form>
                             </div>
                         </div>
-
-                        <p class="text-center text-muted mt-md mb-md">&copy; Copyright 2016. All Rights Reserved.</p>
+                        <div class="alert alert-info">
+                            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                            <strong>Need any help?!</strong> We are glad <a href="" class="alert-link">to help you</a>, if you need.
+                        </div>
+                        <p class="text-center text-muted mt-md mb-md">&copy; Copyright 2017. All Rights Reserved.</p>
                     </div>
+                    
                 </section>
 			</div>
 
