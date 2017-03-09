24acaf15ee2c5d649000e7a67446da0bb4b25b85
Sergio Lopez <sergiolopezm5@users.noreply.github.com>
2017-03-09 16:31:18 +0100

    Creado mailbox
    Creada la pagina para mostrar la lista con los mensajes

---
+++ funciones_php_mailbox.php
@@ -0,0 +1,37 @@
+<?php
+
+function pMostrarListaMensajes() {
+    
+    $devolver = '';
+    
+    for($i = 0; $i < 12; $i++) {
+        
+        $devolver .='<li>
+                        <a href="mailbox-email.html">
+                            <!--<i class="mail-label" style="border-color: #EA4C89"></i>-->
+
+                            <div class="col-sender">
+                                <div class="checkbox-custom checkbox-text-primary ib">
+                                    <input type="checkbox" id="mail2">
+                                    <label for="mail2"></label>
+                                </div>
+                                <p class="m-none ib">Okler Themes</p>
+                            </div>
+                            <div class="col-mail">
+                                <p class="m-none mail-content">
+                                    <span class="subject">Porto Admin theme! &nbsp;–&nbsp;</span>
+                                    <span class="mail-partial">Check it out.</span>
+                                </p>
+                                <!--<i class="mail-attachment fa fa-paperclip"></i>-->
+                                <p class="m-none mail-date">3:35 pm</p>
+                            </div>
+                        </a>
+                    </li>';
+        
+    }
+    
+    return $devolver;
+}
+
+
+?>
