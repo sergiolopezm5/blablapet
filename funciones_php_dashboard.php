8e7b92123e347fd41756bbea2c069d0d6eb6484a
Jamispower <jmg1095@gmail.com>
2017-03-10 15:38:29 +0100

    cambio

--- funciones_php_dashboard.php
+++ funciones_php_dashboard.php
@@ -11,7 +11,7 @@ function pMostrarCuidadores() {
         $devolver .='<div class="col-lg-3 col-sm-6">
                         <div class="plan">
                             <h3>'.$datos['NOMBRE'].'<span>'.$datos['URL_FOTO'].'</span></h3>
-                            <a class="btn btn-lg btn-primary" href="#modalEnviarMensaje">Contactar</a>
+                            <a class="btn btn-lg btn-primary modal-basic" href="#modalEnviarMensaje">Contactar</a>
                             <ul>
                                 <li><b>Provincia: </b>'.$datos['PROVINCIA'].'</li>
                                 <li><b>Ciudad: </b>'.$datos['CIUDAD'].'</li>
