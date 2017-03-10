8ba3184c43cf3d95dc1b1634afa42f036e254fd7
DESKTOP-T5PCDCJ\Marcos Andreo <marcosjesusand@gmail.com>
2017-03-10 19:19:14 +0100

    envio mensajes OK

--- nuevomensaje.php
+++ nuevomensaje.php
@@ -3,8 +3,8 @@
     include("funciones_sql_generales.php");
     include("funciones_php_generales.php");
 
-    include("funciones_sql_mailbox.php");
-    include("funciones_php_mailbox.php");
+    /*include("funciones_sql_mailbox.php");
+    include("funciones_php_mailbox.php");*/
 
 
     sEnviarMensaje($_POST['idemisor'], $_POST['idreceptor'], $_POST['contenido']);
